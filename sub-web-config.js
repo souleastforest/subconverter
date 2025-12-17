654e1a99286e99137c6e5d5a38273602fb5d8d42
Tairitsu_volce_server <tairitsuowo@hotmail.com>
2025-12-18 04:56:30 +0800

    feat: add docker-compose and custom configs

---
+++ sub-web-config.js
@@ -0,0 +1,23 @@
+window.config = {
+    defaultBackend: 'http://12.107.29.194:25500/sub?',
+    remoteConfig: [
+        {
+            label: "FDUZS本地配置",
+            value: "config/fduzs_config.ini"
+        },
+        {
+            label: "ACL4SSR_Online 默认全分组",
+            value: "config/ACL4SSR_Online_Full.ini"
+        }
+    ],
+    backendOptions: [
+        {
+            label: "当前服务器后端",
+            value: "http://12.107.29.194:25500/sub?"
+        },
+        {
+            label: "subconverter.herokuapp.com",
+            value: "https://subconverter.herokuapp.com/sub?"
+        }
+    ]
+}
